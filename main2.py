a0689997ed9ace335bfa645544cd57450eb01b92
Kdj9384 <kdj93843404@gmail.com>
2021-11-15 22:21:51 +0900

    make thread

--- main2.py
+++ main2.py
@@ -1,5 +1,7 @@
-from threading import Thread
+import threading
 from multiprocessing import Process, Queue
+
+from mysql.connector.cursor import MySQLCursor
 from header import g_temperature, g_humidity, g_soil, g_wflow, g_wlvl
 import RPi.GPIO as GPIO
 import sys
@@ -13,6 +15,28 @@ import my_pcf as pcf
 import interaction
 from connect_mysql import setup_DB, insert_executor
 
+rw_mutex = threading.Lock()
+event = threading.Event()
+
+
+def read(mysql_con, mysql_cursor, potID):
+    # rw_mutex.acquire()
+    # read_send_sql(mysql_cursor, mysql_con, potID)
+    # read_dot_matrix()
+    # rw_mutex.release()
+
+    with rw_mutex:
+        read_send_sql(mysql_con, mysql_cursor, potID)
+        read_dot_matrix()
+
+
+def write():
+    # rw_mutex.acquire()
+    # write_global_var()
+    # rw_mutex.release()
+    with rw_mutex:
+        write_global_var()
+
 
 def write_global_var():
     global g_temperature, g_humidity, g_soil, g_wflow, g_wlvl
@@ -22,7 +46,7 @@ def write_global_var():
     g_humidity, g_temperature = dht.readValue()
 
 
-def read_send_sql():
+def read_send_sql(mysql_con, mysql_cursor, potID):
     global g_temperature, g_humidity, g_soil, g_wflow, g_wlvl
     insert_executor(
         mysql_cursor,
@@ -41,6 +65,21 @@ def read_send_sql():
 
 def read_dot_matrix():
     global g_temperature, g_humidity, g_soil, g_wflow, g_wlvl
+    if g_soil < 200:
+        pump.doPumpTime(1)
+    if g_temperature > 30:
+        temp_state = 1
+        interaction.showtempMatrix(temp_state)
+    elif g_temperature < 30:
+        temp_state = 2
+        interaction.showtempMatrix(temp_state)
+    else:
+        temp_state = 0
+        interaction.showtempMatrix(temp_state)
+
+    if g_wlvl < 10:
+        water_state = 1
+        interaction.showwaterMatrix(water_state)
 
 
 def setup_system():
@@ -50,22 +89,18 @@ def setup_system():
     # setup DB connection
     mysql_con, mysql_cursor, potID = setup_DB()
 
-    # make Thread
-    th1 = Thread(target=write_global_var)
-    th2 = Thread(target=read_send_sql, args=(mysql_con, mysql_cursor, potID))
-    th3 = Thread(target=read_dot_matrix, args=())
-
-    # run Thread
+    th1 = threading.Thread(target=write, daemon=True)
     th1.start()
+
+    th2 = threading.Thread(target=read, daemon=True, args=(mysql_con, mysql_cursor, potID))
     th2.start()
-    th3.start()
 
     # make thread for I/O
-    return th1, th2, th3
+    return th1, th2, mysql_con, mysql_cursor
 
 
 if __name__ == "__main__":
-    th_w, th_r_sql, th_r_dot = setup_system()
+    th_w, th_r, mysql_con, mysql_cursor = setup_system()
     try:
         while True:
             print("start")
@@ -77,10 +112,9 @@ if __name__ == "__main__":
     finally:
         GPIO.cleanup()
         mysql_con.close()
-
         th_w.join()
-        th_r_sql.join()
-        th_r_dot.join()
+        th_r.join()
+
 
 else:
     # do nothing
