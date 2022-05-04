7b42b9bc775f93d4d7808721a08e5580fa45d641
강동진 <kangdongjin@gangdongjins-MacBook-Air.local>
2022-05-04 18:35:59 +0900

    modify query

--- connect_mysql.py
+++ connect_mysql.py
@@ -1,3 +1,4 @@
+from main3 import SensorValues
 import mysql.connector
 import time
 import datetime
@@ -5,27 +6,13 @@ import datetime
 
 # connection, cursor, pid를 리턴.
 # 자신의 PID가 DB에 없으면, wait
-def setup_DB():
+def connect_DB():
     pid = 99
-    mysql_con = create_sql_connector("112.170.208.72", "8910", "testDB", "root", "wlgkcjf21gh")
-
-    mysql_cursor = create_sql_cursor(mysql_con)
-
-    while not check_PID(mysql_cursor, pid):
-        print("no such PID")
-        time.sleep(1)
-
-    return mysql_con, mysql_cursor, pid
-
-
-def create_sql_connector(host, port, db, user, pw):
-    mysql_con = mysql.connector.connect(host=host, port=port, database=db, user=user, password=pw)
-    return mysql_con
-
-
-def create_sql_cursor(mysql_con):
+    mysql_con = mysql.connector.connect(
+        host="112.170.208.72", port="8080", database="capstone", user="root", password="wlgkcjf21gh"
+    )
     mysql_cursor = mysql_con.cursor(dictionary=True)
-    return mysql_cursor
+    return mysql_con, mysql_cursor, pid
 
 
 def check_PID(cursor, PID):
@@ -38,19 +25,20 @@ def check_PID(cursor, PID):
         return True
 
 
-def insert_executor(
-    cursor, mysql_con, table_name, PID, Stime, Temp, Humid, SoilMois, Wlevel, Cds, Wflow
-):
+def insert_executor(cursor, mysql_con, table_name, PID, q, SAVE_PATH):
     try:
-        if table_name == "Sensors":
-            sql = "insert into Sensors(PID, Stime, Temp, Humid, SoilMois, Wlevel, Cds, Wflow) VALUES(%s, %s, %s, %s, %s, %s, %s, %s);"
-        elif table_name == "Planter":
-            sql = "insert into Planter(Ptype, Pdate) values(%s, %s);"
+        # TODO parsing code needed
+        if table_name == "Plant":
+            sql = "insert into Plant (PID, ICON, LED, TEMP , HUMID, SOIL, TANK, TIME) VALUES(%s, %s, %s, %s, %s, %s, %s, NOW());"
+            cursor.execute(sql, (PID, q.ICON, q.LED, q.TEMP, q.HUMID, q.SOIL, q.TANK))
+
+        elif table_name == "Gallery":
+            sql = "insert into Gallery (PID, SAVE_DATE, SAVE_PATH) values(%s, NOW(), %s);"
+            cursor.execute(sql, (PID, SAVE_PATH))
 
         else:
             print("no such tables")
 
-        cursor.execute(sql, (PID, Stime, Temp, Humid, SoilMois, Wlevel, Cds, Wflow))
         mysql_con.commit()
 
     except mysql.connector.errors.OperationalError as e:
@@ -61,18 +49,18 @@ def insert_executor(
 if __name__ == "__main__":
     try:
         print("start")
-        mysql_con, mysql_cursor, pid = setup_DB()
+        mysql_con, mysql_cursor, pid = connect_DB()
 
-        # insert_executor(mysql_cursor, "Planter",
-        #                 "param1", "2021-11-7 12:53:33")
+        # for i in range(10):
+        #     currtime = datetime.datetime.now()
+        #     print(currtime)
+        #     insert_executor(
+        #         mysql_cursor, mysql_con, "Sensors", str(pid), str(currtime), 99, 99, 99, 99, 99, 99
+        #     )
+        #     time.sleep(1)
+        q = SensorValues(-1, -1, -1, -1)
+        insert_executor(mysql_con, mysql_cursor, "Plant", pid, q, f"/images/{pid}")
 
-        for i in range(10):
-            currtime = datetime.datetime.now()
-            print(currtime)
-            insert_executor(
-                mysql_cursor, mysql_con, "Sensors", str(pid), str(currtime), 99, 99, 99, 99, 99, 99
-            )
-            time.sleep(1)
         print("query_executor")
 
         mysql_cursor.close()
---
+++ main4.py
@@ -0,0 +1,158 @@
+import threading
+import RPi.GPIO as GPIO
+import sys
+import time
+import datetime
+from dataclasses import dataclass
+import queue
+
+import my_dht11 as dht
+import my_pump as pump
+import my_pcf as pcf
+from connect_mysql import setup_DB, insert_executor
+
+que = queue.Queue(2048)
+eve_r = threading.Event()
+eve_w = threading.Event()
+
+
+@dataclass
+class SensorValues:
+    TEMP: int = None
+    HUMID: int = None
+    SOIL: int = None
+    TANK: int = None
+
+    def __init__(self, temp, humid, soil, wlvl):
+        self.TEMP = temp
+        self.HUMID = humid
+        self.SOIL = soil
+        self.TANK = wlvl
+
+
+def write():
+    try:
+        while True:
+            if eve_w.is_set():
+                return 0
+            write_global_var()
+    except:
+        return 0
+    finally:
+        print("writer finally")
+
+
+def read(mysql_con, mysql_cursor, potID):
+    try:
+        while True:
+            if eve_r.is_set():
+                return 0
+
+            # if que is empty, it will blocked for 100sec
+            # after 100sec, it will occur "queue.Empty" exception
+            # ** if block=True & timeout=None, this operation can't stop, it is not occur keyboardexception
+            q = que.get(block=True, timeout=100)
+
+            # 가져온 10개의 데이터의 평균을 로컬로 저장
+
+            # 로컬 값을 쏴주기
+            print(
+                "temp:{} humid:{} soil:{} wlvl:{}".format(
+                    q.TEMP,
+                    q.HUMID,
+                    q.SOIL,
+                    q.TANK,
+                )
+            )
+
+            read_send_sql(mysql_con, mysql_cursor, potID, q)
+
+            que.task_done()
+            time.sleep(1)
+
+    except OSError:
+        return 0
+
+    finally:
+        # mysql_con.close()
+        print("reader finally")
+
+
+def write_global_var():
+    SOIL = pcf.getSoilMoisture()
+    TANK = pcf.getWaterLevel()
+    HUMID, TEMP = dht.readValue()
+    temp = SensorValues(TEMP, HUMID, SOIL, TANK)
+
+    # if que is full, it will blocked 100sec
+    # after 100sec, it occur "queue.Full" exception
+    que.put(temp, block=True, timeout=100)
+
+
+def read_send_sql(mysql_con, mysql_cursor, PID, q):
+    insert_executor(
+        mysql_cursor,
+        mysql_con,
+        "Plant",
+        PID,
+        # datetime.datetime.now(),
+        q,
+        f"/images/{PID}",
+    )
+    insert_executor(
+        mysql_cursor,
+        mysql_con,
+        "Gallery",
+        PID,
+        # datetime.datetime.now(),
+        q,
+        f"/images/{PID}",
+    )
+
+
+def setup_():
+    # setup DB connection
+    mysql_con, mysql_cursor, PID = setup_DB()
+
+    th1 = threading.Thread(target=write, daemon=True)
+    th1.start()
+
+    th2 = threading.Thread(target=read, daemon=True, args=(mysql_con, mysql_cursor, PID))
+    th2.start()
+
+    # make thread for I/O
+    return th1, th2, mysql_con, mysql_cursor
+
+
+if __name__ == "__main__":
+    th_w, th_r, mysql_con, mysql_cursor = setup_()
+    try:
+        while True:
+            print("main start, 10sec sleep")
+            time.sleep(10)
+
+    finally:
+        print("cleanup started")
+
+        # kill write
+        eve_w.set()
+        th_w.join()
+
+        # free GPIO
+        GPIO.cleanup()
+
+        # wait until queue is empty
+        que.join()
+
+        # kill read
+        eve_r.set()
+        th_r.join()
+
+        # free connection
+        mysql_cursor.close()
+        mysql_con.close()
+
+
+else:
+    # do nothing
+    print("This do nothing")
